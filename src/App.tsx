e2256ae280db5b8ccdb62ec90e4bd10a1eaaeb01
ilaburlak <ilya.burlak.burlak@mail.ru>
2026-08-16 21:32:19 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { observer } from 'mobx-react-lite'
+import './App.scss'
+
+export const App = observer(() => {
+  return (
+      <>
+          hi
+          hi
+          ih
+
+      </>
+  )
+
+})
+
